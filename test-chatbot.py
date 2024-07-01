af89d027aeed25c27036b3b1678e5b59f7042e69
Lindsayzhangg <169024298+Lindsayzhangg@users.noreply.github.com>
2024-07-01 19:55:29 +0800

    Update test-chatbot.py

--- test-chatbot.py
+++ test-chatbot.py
@@ -1,3 +1,41 @@
+import streamlit as st
+from langchain_voyageai import VoyageAIEmbeddings
+import os
+import boto3
+from urllib.parse import urlparse
+import pinecone
+from langchain_openai import ChatOpenAI
+from langchain.chains import LLMChain, RetrievalQA
+import time
+import re
+from langchain_pinecone import PineconeVectorStore
+from langchain.memory import ConversationBufferMemory
+from langchain.schema import HumanMessage
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import ConversationChain
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+import uuid
+import warnings
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain import hub
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_pinecone import PineconeVectorStore
+from langchain_voyageai import VoyageAIEmbeddings
+from langchain.chains import create_history_aware_retriever
+from dotenv import load_dotenv
+
+# Ignore all warnings
+warnings.filterwarnings("ignore")
+
+# Set up Streamlit app
+st.set_page_config(page_title="Custom Chatbot", layout="wide")
+st.title("Custom Chatbot with Retrieval Abilities")
+
 # Function to generate pre-signed URL
 def generate_presigned_url(s3_uri):
     parsed_url = urlparse(s3_uri)
@@ -27,6 +65,95 @@ def retrieve_and_format_response(query, retriever, llm):
     for doc in docs:
         st.write(f"Document ID: {doc.metadata['id']}, Content: {doc.page_content[:100]}...")
     return combined_content
+
+# Setup - Streamlit secrets
+OPENAI_API_KEY = st.secrets["api_keys"]["OPENAI_API_KEY"]
+VOYAGE_AI_API_KEY = st.secrets["api_keys"]["VOYAGE_AI_API_KEY"]
+PINECONE_API_KEY = st.secrets["api_keys"]["PINECONE_API_KEY"]
+aws_access_key_id = st.secrets["aws"]["aws_access_key_id"]
+aws_secret_access_key = st.secrets["aws"]["aws_secret_access_key"]
+aws_region = st.secrets["aws"]["aws_region"]
+
+# Pull the retrieval QA chat prompt
+retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+llm = ChatOpenAI(model="gpt-4o", openai_api_key=OPENAI_API_KEY)
+
+# Initialize the retriever using PineconeVectorStore
+model_name = "voyage-large-2"
+embedding_function = VoyageAIEmbeddings(
+    model=model_name,
+    voyage_api_key= VOYAGE_AI_API_KEY
+)
+vector_store = PineconeVectorStore.from_existing_index(
+    embedding=embedding_function,
+    index_name="test"
+)
+retriever = vector_store.as_retriever()
+
+contextualize_q_system_prompt = (
+    "Given a chat history and the latest user question "
+    "which might reference context in the chat history, "
+    "formulate a standalone question which can be understood "
+    "without the chat history. Do NOT answer the question, "
+    "just reformulate it if needed and otherwise return it as is."
+)
+
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
+)
+
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
+
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
+
+rag_retreival_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+# Initialize memory
+memory = ConversationBufferMemory()
+
+# Store for chat history
+store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_retreival_chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer",
+)
+
+def retrieve_and_format_response(user_input, retriever, llm):
+    response = conversational_rag_chain.invoke({"input": user_input}, config={"configurable": {"session_id": "test"}})
+    return response
 
 # Functions to save chat history and upload to S3
 def save_chat_history_to_file(filename, history):
